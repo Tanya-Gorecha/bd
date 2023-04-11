f154049cc80fe76a8658871155bb8551f930cb8f
Tanya-Gorecha <121540704+Tanya-Gorecha@users.noreply.github.com>
2023-04-11 18:22:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-	fabric.Image.fromURL('BirthdayImage.jpg',function(img){
+	fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
         block_image_object = Img ;
         block_image_object.scaleToWidth(700);
-        block_image_object.caleToHeight(510)  ;
+        block_image_object.scaleToHeight(510)  ;
         block_image_object.set({
             top:0,
             left:0
